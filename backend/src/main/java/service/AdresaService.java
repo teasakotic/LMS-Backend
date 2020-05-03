8e6296af62958c2b0ef7cdc6b4633ffec2863895
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-03 16:43:25 +0200

    Update AdresaService.java

--- backend/src/main/java/service/AdresaService.java
+++ backend/src/main/java/service/AdresaService.java
@@ -1,5 +1,44 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Adresa;
+import repository.AdresaRepository;
+
+@Service 
 public class AdresaService {
+
+	@Autowired
+	AdresaRepository adresaRepo;
+	
+	public Iterable<Adresa> getAdresa() {
+		return adresaRepo.findAll();
+	}
+	
+	public Optional<Adresa> getOne(Long id) {
+		return adresaRepo.findById(id);
+	}
+	
+	public void addAdresa(Adresa adresa) {
+		adresaRepo.save(adresa);
+	}
+	
+	public void removeAdresa(Long id) {
+		Optional<Adresa> adresss= adresaRepo.findById(id);
+		if(adresss.isPresent()) {
+			adresaRepo.delete(adresss.get());
+		}
+	}
+	
+	public void updateAdresa(Long id, Adresa adresa) {
+		Optional<Adresa> adresss = adresaRepo.findById(id);
+		if(adresss.isPresent()) {
+			adresa.setId(adresss.get().getId());
+			adresaRepo.save(adresa);
+		}
+	}
 
 }
