ed83d5ea97954f7a6e19a03b0526a7a4ceb3dbda
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-03 16:44:19 +0200

    Update AdresaRepository.java

--- backend/src/main/java/repository/AdresaRepository.java
+++ backend/src/main/java/repository/AdresaRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class AdresaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import model.Adresa;
 
-}
+@Repository 
+public interface AdresaRepository extends JpaRepository<Adresa, Long> {
+
+} 
+
