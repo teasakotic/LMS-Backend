6421d6a5abb4ee8ccd232e3117bcbc5abda29617
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-03 17:04:03 +0200

    Update KategorijaRepository.java

--- backend/src/main/java/repository/KategorijaRepository.java
+++ backend/src/main/java/repository/KategorijaRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class KategorijaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Kategorija;
+
+@Repository
+public interface KategorijaRepository extends JpaRepository<Kategorija, Long> {
 
 }
