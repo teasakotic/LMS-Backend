a02b54589d945cb1e3b2f9933bb435125d79f082
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:57:28 +0200

    Update EvaluacijaZnanjaIshodRepository.java

--- backend/src/main/java/repository/EvaluacijaZnanjaIshodRepository.java
+++ backend/src/main/java/repository/EvaluacijaZnanjaIshodRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class EvaluacijaZnanjaIshodRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.EvaluacijaZnanjaIshod;
+
+@Repository
+public interface EvaluacijaZnanjaIshodRepository extends JpaRepository<EvaluacijaZnanjaIshod, Long> {
 
 }
