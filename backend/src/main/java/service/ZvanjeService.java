77f578fd7d4166b6ebe374aa8fe3b3cf04fd128c
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:58:55 +0200

    Zvanje

--- backend/src/main/java/service/ZvanjeService.java
+++ backend/src/main/java/service/ZvanjeService.java
@@ -1,5 +1,45 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Zvanje;
+import repository.ZvanjeRepository;
+
+@Service
 public class ZvanjeService {
+
+    @Autowired
+    private ZvanjeRepository zvanjeRepo;
+
+    public ZvanjeService() {
+    }
+
+    public Iterable<Zvanje> getZvanje() {
+        return zvanjeRepo.findAll();
+    }
+
+    public Optional<Zvanje> getZvanjeById(Long id) {
+        return zvanjeRepo.findById(id);
+    }
+
+    public void addZvanje(Zvanje zvanje) {
+    	zvanjeRepo.save(zvanje);
+    }
+
+    public void removeZvanje(Long id) {
+        Optional<Zvanje> zvanje = zvanjeRepo.findById(id);
+        zvanjeRepo.delete(zvanje.get());
+    }
+
+	public void updateZvanje(Long id,Zvanje zvanje) {
+		Optional<Zvanje> zva = zvanjeRepo.findById(id);
+		if(zva.isPresent()) {
+			zvanje.setId(zva.get().getId());
+			zvanjeRepo.save(zvanje);
+		}
+	}
 
 }
