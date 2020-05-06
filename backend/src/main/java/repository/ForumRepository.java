89db7fdfdb528eb3d81a9ca3e3e739d7a63392aa
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 20:40:29 +0200

    Update ForumRepository.java

--- backend/src/main/java/repository/ForumRepository.java
+++ backend/src/main/java/repository/ForumRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class ForumRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Forum;
+
+@Repository 
+public interface ForumRepository extends JpaRepository<Forum, Long> {
 
 }
