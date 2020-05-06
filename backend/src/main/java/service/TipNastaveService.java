484bc8fe59dab388f3c7e2172920124bb62ac7e1
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:43:21 +0200

    TipNastave

--- backend/src/main/java/service/TipNastaveService.java
+++ backend/src/main/java/service/TipNastaveService.java
@@ -1,5 +1,43 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.ObjavaFajl;
+import model.TipNastave;
+import repository.TipNastaveRepository;
+
+@Service 
 public class TipNastaveService {
+
+	@Autowired
+	TipNastaveRepository tipNastaveRepo;
+	
+	public Iterable<TipNastave> getTipNastave() {
+		return tipNastaveRepo.findAll();
+	}
+	
+	public Optional<TipNastave> getOne(Long id) {
+		return tipNastaveRepo.findById(id);
+	}
+	
+	public void addTipNastave(TipNastave tipNastave) {
+		tipNastaveRepo.save(tipNastave);
+	}
+	
+    public void removeTipNastave(Long id) {
+        Optional<TipNastave> tipNastave = tipNastaveRepo.findById(id);
+        tipNastaveRepo.delete(tipNastave.get());
+    }
+
+    public void updateTipNastave(Long id, TipNastave tipNastave) {
+        Optional<TipNastave> Nas = tipNastaveRepo.findById(id);
+        if(Nas.isPresent()) {
+            tipNastave.setId(Nas.get().getId());
+            tipNastaveRepo.save(tipNastave);
+        }
+    }
 
 }
