1e6c4b28a9e7796396d74bdee664dd9f84e3c8c2
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 16:21:51 +0200

    Univerzitet

--- backend/src/main/java/repository/UniverzitetRepository.java
+++ backend/src/main/java/repository/UniverzitetRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class UniverzitetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Univerzitet;
+
+@Repository
+public interface UniverzitetRepository extends JpaRepository<Univerzitet, Long> {
 
 }
