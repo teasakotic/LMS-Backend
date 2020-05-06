5c4661ff29b6889ef11e59fcd6c8bf9b48c994a8
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:27:40 +0200

    Update UlogaRepository.java

--- backend/src/main/java/repository/UlogaRepository.java
+++ backend/src/main/java/repository/UlogaRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class UlogaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Uloga;
+
+@Repository
+public interface UlogaRepository extends JpaRepository<Uloga, Long> {
 
 }
