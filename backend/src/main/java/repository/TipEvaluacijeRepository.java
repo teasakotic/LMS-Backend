af598a3498f89d72ff92d764d415fc9433149038
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 14:45:26 +0200

    Update TipEvaluacijeRepository.java

--- backend/src/main/java/repository/TipEvaluacijeRepository.java
+++ backend/src/main/java/repository/TipEvaluacijeRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class TipEvaluacijeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import model.TipEvaluacije;
+
+@Repository
+public interface TipEvaluacijeRepository extends JpaRepository<TipEvaluacije, Long> {
+	
 }
