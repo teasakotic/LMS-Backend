6d57cc2625326b30095c792f29c62c1cd50ddff4
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:28:33 +0200

    Update UlogaService.java

--- backend/src/main/java/service/UlogaService.java
+++ backend/src/main/java/service/UlogaService.java
@@ -1,5 +1,34 @@
 package service;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Uloga;
+import repository.UlogaRepository;
+
+@Service 
 public class UlogaService {
 
+	@Autowired
+	UlogaRepository ulogaRepo;
+	
+	public Iterable<Uloga> getUloga() {
+		return ulogaRepo.findAll();
+	}
+	
+	public Optional<Uloga> getOne(Long id) {
+		return ulogaRepo.findById(id);
+	}
+	
+	public void addUloga(Uloga uloga) {
+		ulogaRepo.save(uloga);
+	}
+	
+	public void removeUloga(Long id) {
+		Optional<Uloga> ulg = ulogaRepo.findById(id);
+		if(ulg.isPresent()) {
+			ulogaRepo.delete(ulg.get());
+		}
+	}
 }
