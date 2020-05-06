e330efcfb7901b0afaf752aba7529834ea5c75b0
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 14:49:32 +0200

    EmailFakulteta

--- backend/src/main/java/repository/EmailFakultetaRepository.java
+++ backend/src/main/java/repository/EmailFakultetaRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public class EmailFakultetaRepository {
+import java.util.ArrayList;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.EmailFakulteta;
+
+@Repository
+public interface EmailFakultetaRepository extends JpaRepository<EmailFakulteta, Long> {
+	ArrayList<EmailFakulteta> findByFakultetIdEquals(Long id);
 }
