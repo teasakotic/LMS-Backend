8f5591ec587eef12e36f2eb7205e5a079c99e7f5
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:16:59 +0200

    Update TemaRepository.java

--- backend/src/main/java/repository/TemaRepository.java
+++ backend/src/main/java/repository/TemaRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class TemaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.Tema;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
 
 }
