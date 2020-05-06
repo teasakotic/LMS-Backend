49fe077159a90c07958ffa809cc79bffbc13228e
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 23:04:40 +0200

    Update ForumKorisnikForumUlogaRepository.java

--- backend/src/main/java/repository/ForumKorisnikForumUlogaRepository.java
+++ backend/src/main/java/repository/ForumKorisnikForumUlogaRepository.java
@@ -1,5 +1,12 @@
 package repository;
 
-public class ForumKorisnikForumUlogaRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.ForumKorisnikForumUloga;
+
+@Repository
+public interface ForumKorisnikForumUlogaRepository extends JpaRepository<ForumKorisnikForumUloga, Long> {
 
 }
