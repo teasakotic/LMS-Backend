470e377da40c0cdcf3390927cb00c4109c791587
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:35:04 +0200

    Update FakultetService.java

--- backend/src/main/java/service/FakultetService.java
+++ backend/src/main/java/service/FakultetService.java
@@ -1,5 +1,35 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Fakultet;
+import repository.FakultetRepository;
+
+@Service 
 public class FakultetService {
 
+	@Autowired
+	FakultetRepository fakultetRepo;
+	
+	public Iterable<Fakultet> getFakultet() {
+		return fakultetRepo.findAll();
+	}
+	
+	public Optional<Fakultet> getOne(Long id) {
+		return fakultetRepo.findById(id);
+	}
+	
+	public void addFakultet(Fakultet fakultet) {
+		fakultetRepo.save(fakultet);
+	}
+	
+	public void removeFakultet(Long id) {
+		Optional<Fakultet> faks = fakultetRepo.findById(id);
+		if(faks.isPresent()) {
+			fakultetRepo.delete(faks.get());
+		}
+	}
 }
