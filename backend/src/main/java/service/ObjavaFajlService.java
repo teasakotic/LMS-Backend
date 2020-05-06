c972e9c10a2ee659501a0f316fbeb786d9452e8c
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:51:21 +0200

    Update ObjavaFajlService.java

--- backend/src/main/java/service/ObjavaFajlService.java
+++ backend/src/main/java/service/ObjavaFajlService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import model.ObjavaFajl;
-import repository.ObjavaFajlRepository;;
+import repository.ObjavaFajlRepository;
 
 @Service
 public class ObjavaFajlService {
