671ebdbfaaf1366f4af6594c92f5c7088b266363
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:06:50 +0200

    ObjavaFajl

--- backend/src/main/java/service/ObjavaFajlService.java
+++ backend/src/main/java/service/ObjavaFajlService.java
@@ -1,5 +1,45 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.ObjavaFajl;
+import repository.ObjavaFajlRepository;;
+
+@Service
 public class ObjavaFajlService {
+
+    @Autowired
+    private ObjavaFajlRepository objavaFajlRepo;
+
+    public ObjavaFajlService() {
+    }
+
+    public Iterable<ObjavaFajl> getObjavaFajl() {
+        return objavaFajlRepo.findAll();
+    }
+
+    public Optional<ObjavaFajl> getObjavaFajlById(Long id) {
+        return objavaFajlRepo.findById(id);
+    }
+
+    public void addObjavaFajl(ObjavaFajl objavaFajl) {
+    	objavaFajlRepo.save(objavaFajl);
+    }
+
+    public void removeObjavaFajl(Long id) {
+        Optional<ObjavaFajl> objavaFajl = objavaFajlRepo.findById(id);
+        objavaFajlRepo.delete(objavaFajl.get());
+    }
+
+	public void updateObjavaFajl(Long id, ObjavaFajl objavaFajl) {
+		Optional<ObjavaFajl> obj = objavaFajlRepo.findById(id);
+		if(obj.isPresent()) {
+			objavaFajl.setId(obj.get().getId());
+			objavaFajlRepo.save(objavaFajl);
+		}
+	}
 
 }
