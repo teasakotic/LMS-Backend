fcfd4ca0000ebfb7ef5448781958f6893194b84f
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 16:09:09 +0200

    TipZvanja

--- backend/src/main/java/repository/TipZvanjaRepository.java
+++ backend/src/main/java/repository/TipZvanjaRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class TipZvanjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.TipZvanja;
+
+@Repository
+public interface TipZvanjaRepository extends JpaRepository<TipZvanja, Long> {
 
 }
