804d97c73a768e4988784b205cb78a7195bcd13b
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:23:38 +0200

    Update DrzavaService.java

--- backend/src/main/java/service/DrzavaService.java
+++ backend/src/main/java/service/DrzavaService.java
@@ -1,5 +1,35 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Drzava;
+import repository.DrzavaRepository;
+
+@Service 
 public class DrzavaService {
 
+	@Autowired
+	DrzavaRepository drzavaRepo;
+	
+	public Iterable<Drzava> getDrzava() {
+		return drzavaRepo.findAll();
+	}
+	
+	public Optional<Drzava> getOne(Long id) {
+		return drzavaRepo.findById(id);
+	}
+	
+	public void addDrzava(Drzava drzava) {
+		drzavaRepo.save(drzava);
+	}
+	
+	public void removeDrzava(Long id) {
+		Optional<Drzava> drz = drzavaRepo.findById(id);
+		if(drz.isPresent()) {
+			drzavaRepo.delete(drz.get());
+		}
+	}
 }
