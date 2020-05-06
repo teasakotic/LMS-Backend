6b54efc9b6b3e8befa1b7432e8315eb56b119ddf
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 16:08:33 +0200

    TipZvanja

--- backend/src/main/java/service/TipZvanjaService.java
+++ backend/src/main/java/service/TipZvanjaService.java
@@ -1,5 +1,45 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.TipZvanja;
+import repository.TipZvanjaRepository;
+
+@Service
 public class TipZvanjaService {
+
+    @Autowired
+    private TipZvanjaRepository tipZvanjaRepo;
+
+    public TipZvanjaService() {
+    }
+
+    public Iterable<TipZvanja> getTipZvanja() {
+        return tipZvanjaRepo.findAll();
+    }
+
+    public Optional<TipZvanja> getTipZvanjaById(Long id) {
+        return tipZvanjaRepo.findById(id);
+    }
+
+    public void addTipZvanja(TipZvanja tipZvanja) {
+    	tipZvanjaRepo.save(tipZvanja);
+    }
+
+    public void removeTipZvanja(Long id) {
+        Optional<TipZvanja> tipZvanja = tipZvanjaRepo.findById(id);
+        tipZvanjaRepo.delete(tipZvanja.get());
+    }
+
+	public void updateTipZvanja(Long id, TipZvanja tipZvanja) {
+		Optional<TipZvanja> tzv = tipZvanjaRepo.findById(id);
+		if(tzv.isPresent()) {
+			tipZvanja.setId(tzv.get().getId());
+			tipZvanjaRepo.save(tipZvanja);
+		}
+	}
 
 }
