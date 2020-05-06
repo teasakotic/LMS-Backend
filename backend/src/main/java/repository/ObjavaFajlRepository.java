70e1ae5f1966e29e50dabe0d95b8a840ec58a2c3
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 14:54:29 +0200

    Update ObjavaFajlRepository.java

--- backend/src/main/java/repository/ObjavaFajlRepository.java
+++ backend/src/main/java/repository/ObjavaFajlRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class ObjavaFajlRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.ObjavaFajl;;
+
+@Repository
+public interface ObjavaFajlRepository extends JpaRepository<ObjavaFajl, Long> {
 
 }
