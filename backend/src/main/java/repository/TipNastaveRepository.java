e9892524b2c3bdd91662e6e8c4093cca2429b9bc
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:44:05 +0200

    Update TipNastaveRepository.java

--- backend/src/main/java/repository/TipNastaveRepository.java
+++ backend/src/main/java/repository/TipNastaveRepository.java
@@ -1,5 +1,11 @@
 package repository;
 
-public class TipNastaveRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.TipNastave;
+
+@Repository
+public interface TipNastaveRepository extends JpaRepository<TipNastave, Long> {
 
 }
