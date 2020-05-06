f9730fc125f6ce719d995f1abbbdb41f6d9fe13c
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 15:48:40 +0200

    Update TelefoniFakultetaRepository.java

--- backend/src/main/java/repository/TelefoniFakultetaRepository.java
+++ backend/src/main/java/repository/TelefoniFakultetaRepository.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import model.TelefoniFakulteta;;
+import model.TelefoniFakulteta;
 
 @Repository
 public interface TelefoniFakultetaRepository extends JpaRepository<TelefoniFakulteta, Long> {
