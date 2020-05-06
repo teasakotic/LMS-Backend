2cd2f579272395184b4d3c6a5d3447c193984efc
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 20:13:33 +0200

    Update MestoRepository.java

--- backend/src/main/java/repository/MestoRepository.java
+++ backend/src/main/java/repository/MestoRepository.java
@@ -1,5 +1,10 @@
 package repository;
 
-public class MestoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import model.Mesto;
 
-}
+@Repository 
+public interface MestoRepository extends JpaRepository<Mesto, Long> {
+
+} 
