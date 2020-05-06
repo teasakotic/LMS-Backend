d12a0cd5d174c1e0b9b77b31662cc626a8a1fa57
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 20:06:54 +0200

    Update KategorijaUlogaRepository.java

--- backend/src/main/java/repository/KategorijaUlogaRepository.java
+++ backend/src/main/java/repository/KategorijaUlogaRepository.java
@@ -1,5 +1,10 @@
 package repository;
 
-public class KategorijaUlogaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import model.KategorijaUloga;
+
+@Repository 
+public interface KategorijaUlogaRepository extends JpaRepository<KategorijaUloga, Long> {
 
 }
