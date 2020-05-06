91c06548c5b6bcf24f2f4eabd2af5fb26af2029e
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 16:15:16 +0200

    TelefoniUniverziteta

--- backend/src/main/java/repository/TelefoniUniverzitetaRepository.java
+++ backend/src/main/java/repository/TelefoniUniverzitetaRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public class TelefoniUniverzitetaRepository {
+import java.util.ArrayList;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.TelefoniUniverziteta;
+
+@Repository
+public interface TelefoniUniverzitetaRepository extends JpaRepository<TelefoniUniverziteta, Long> {
+	ArrayList<TelefoniUniverziteta> findByUniverzitetIdEquals(Long id);
 }
