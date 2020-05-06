9817db1d81f088ca96b92d297dcbce070fe1745c
Zeljanaa <63678998+Zeljanaa@users.noreply.github.com>
2020-05-06 22:58:43 +0200

    Update EvaluacijaZnanjaIshodService.java

--- backend/src/main/java/service/EvaluacijaZnanjaIshodService.java
+++ backend/src/main/java/service/EvaluacijaZnanjaIshodService.java
@@ -1,5 +1,43 @@
 package service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.EvaluacijaZnanjaIshod;
+import repository.EvaluacijaZnanjaIshodRepository;
+
+@Service 
 public class EvaluacijaZnanjaIshodService {
+
+	@Autowired
+	EvaluacijaZnanjaIshodRepository evaluacijaZnanjaIshodRepo;
+	
+	public Iterable<EvaluacijaZnanjaIshod> getEvaluacijaZnanjaIshod() {
+		return evaluacijaZnanjaIshodRepo.findAll();
+	}
+	
+	public Optional<EvaluacijaZnanjaIshod> getOne(Long id) {
+		return evaluacijaZnanjaIshodRepo.findById(id);
+	}
+	
+	public void addEvaluacijaZnanjaIshod(EvaluacijaZnanjaIshod evaluacijaZnanjaIshod) {
+		evaluacijaZnanjaIshodRepo.save(evaluacijaZnanjaIshod);
+	}
+	
+	public void removeEvaluacijaZnanjaIshod(Long id) {
+		Optional<EvaluacijaZnanjaIshod> evl = evaluacijaZnanjaIshodRepo.findById(id);
+		if(evl.isPresent()) {
+			evaluacijaZnanjaIshodRepo.delete(evl.get());
+		}
+	}
+	public void updateEvaluacijaZnanjaIshod(Long id, EvaluacijaZnanjaIshod evaluacijaZnanjaIshod) {
+		Optional<EvaluacijaZnanjaIshod> evl = evaluacijaZnanjaIshodRepo.findById(id);
+		if(evl.isPresent()) {
+			evaluacijaZnanjaIshod.setId(evl.get().getId());
+			evaluacijaZnanjaIshodRepo.save(evaluacijaZnanjaIshod);
+		}
+	}
 
 }
