be0e10e884056210629baceca0603ea0e9a6975e
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-05-06 16:13:46 +0200

    EmailUniverziteta

--- backend/src/main/java/repository/EmailUniverzitetaRepository.java
+++ backend/src/main/java/repository/EmailUniverzitetaRepository.java
@@ -1,5 +1,13 @@
 package repository;
 
-public class EmailUniverzitetaRepository {
+import java.util.ArrayList;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import model.EmailUniverziteta;
+
+@Repository
+public interface EmailUniverzitetaRepository extends JpaRepository<EmailUniverziteta, Long> {
+	ArrayList<EmailUniverziteta> findByUniverzitetIdEquals(Long id);
 }
