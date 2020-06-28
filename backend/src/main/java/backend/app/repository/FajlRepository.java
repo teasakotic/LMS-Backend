0763889001e852834bd6670634a8d3768721ce0a
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-06-28 19:57:10 +0200

    dodat fajl

--- backend/src/main/java/backend/app/repository/FajlRepository.java
+++ backend/src/main/java/backend/app/repository/FajlRepository.java
@@ -1,5 +1,14 @@
 package backend.app.repository;
 
-public class FajlRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import backend.app.model.Fajl;
+
+@Repository
+public interface FajlRepository extends JpaRepository<Fajl, Long> {
+	@Query("SELECT f FROM Fajl f WHERE f.nastavniMaterijal.realizacijaPredmeta.predmet.id = ?1")
+	Iterable<Optional<Fajl>> getAllByPredmet(Long id);
 }
