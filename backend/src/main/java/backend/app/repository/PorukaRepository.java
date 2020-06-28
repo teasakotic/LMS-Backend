924549245c032c133dd54a90e35160191e40409c
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-06-28 20:34:35 +0200

    update

--- backend/src/main/java/backend/app/repository/PorukaRepository.java
+++ backend/src/main/java/backend/app/repository/PorukaRepository.java
@@ -1,5 +1,10 @@
 package backend.app.repository;
 
-public class PorukaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import backend.app.model.Poruka;
 
+@Repository
+public interface PorukaRepository extends JpaRepository<Poruka, Long> {
+	Iterable<Poruka> findByPrimalacOrPosiljalac(String primalac, String posiljalac);
 }
