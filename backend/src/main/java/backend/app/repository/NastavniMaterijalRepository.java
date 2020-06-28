9b95fabd794809ebbd3f17dc64e4310bebbd67c3
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-06-28 20:16:48 +0200

    update

--- backend/src/main/java/backend/app/repository/NastavniMaterijalRepository.java
+++ backend/src/main/java/backend/app/repository/NastavniMaterijalRepository.java
@@ -1,5 +1,14 @@
 package backend.app.repository;
 
-public class NastavniMaterijalRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import backend.app.model.NastavniMaterijal;
+
+@Repository
+public interface NastavniMaterijalRepository extends JpaRepository<NastavniMaterijal, Long> {
+	@Query("SELECT t FROM NastavniMaterijal t WHERE t.realizacijaPredmeta.predmet.id = ?1")
+	Iterable<Optional<NastavniMaterijal>> getAllByPredmet(Long id);
 }
