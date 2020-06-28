6c59d8dee1506a54353e24b7dd5103b5234c26e2
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-06-28 20:40:02 +0200

    update

--- backend/src/main/java/backend/app/repository/TerminNastaveRepository.java
+++ backend/src/main/java/backend/app/repository/TerminNastaveRepository.java
@@ -1,5 +1,15 @@
 package backend.app.repository;
 
-public class TerminNastaveRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import backend.app.model.TerminNastave;
+
+@Repository
+public interface TerminNastaveRepository extends JpaRepository<TerminNastave, Long> {
+	@Query("SELECT tt FROM  TerminNastave tt WHERE tt.realizacijaPredmeta.godinaStudija.id = ?1")
+	Iterable<Optional<TerminNastave>> getByGodinaStudija(Long godinaStudijaId);
 }
