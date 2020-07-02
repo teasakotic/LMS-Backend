f4a930ba26143168e92fea046bb2c1c37d591212
Teodora Šakotić <58300977+teasakotic@users.noreply.github.com>
2020-07-02 19:14:55 +0200

    update

--- backend/src/main/java/backend/app/App.java
+++ backend/src/main/java/backend/app/App.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"app.backend.dependency.exception"})
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
